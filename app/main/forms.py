c7d3c722db45fbbf4485007c75a7f84192ef530c
何钦尧 <hqythu@gmail.com>
2017-05-13 15:33:38 +0800

    basic form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,13 +1,17 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms import SubmitField
-from wtforms import RadioField
+from wtforms import SelectField
 from wtforms.validators import DataRequired
 
+from .defs import candidates
 
-class VotingForm(Form):
-    selection = RadioField('投票',
-                           choices=[],
-                           validators=[DataRequired()])
+blank_option = ('', ' -- select an option -- ')
+
+
+class VotingForm(FlaskForm):
+    selection = SelectField('投票',
+                            choices=[blank_option] + candidates,
+                            validators=[DataRequired()])
     token = StringField('Token', validators=[DataRequired()])
     submit = SubmitField('提交')
--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from flask import request
 from flask import render_template
 
@@ -5,7 +7,24 @@ from app.main import main_blueprint
 from .forms import VotingForm
 
 
-@main_blueprint.route('/', methods=['get', 'post'])
+@main_blueprint.route('/', methods=['GET', 'POST'])
 def index():
     form = VotingForm()
-    return render_template('submit.html', form=form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            if form.selection.data == '':
+                return render_template('result.html', message='请选择正确的项')
+            selection = form.selection.data
+            token = form.token.data
+            print(selection, token, flush=True)
+            print('ok', flush=True)
+        else:
+            print('err', flush=True)
+            return render_template('result.html', message='提交的表单填写有误')
+    else:
+        return render_template('submit.html', form=form)
+
+
+@main_blueprint.route('/result', methods=['GET'])
+def result():
+    pass
