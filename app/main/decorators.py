0db9675044ea51078de3e541613526acbb423d22
何钦尧 <hqythu@gmail.com>
2017-05-13 16:01:16 +0800

    time limit decorator

---
+++ app/main/decorators.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from datetime import timezone
+from datetime import timedelta
+import functools
+
+from flask import request
+from flask import render_template
+
+
+def time_limits(start_time, end_time,
+                start_time_message=None,
+                end_time_message=None):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            now = datetime.now(tz=timezone(timedelta(hours=8)))
+            print(now, flush=True)
+            if start_time is not None and now < start_time:
+                return render_template('result.html',
+                                       message=start_time_message)
+            if end_time is not None and now > end_time:
+                return render_template('result.html', message=end_time_message)
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,21 @@
 import logging
+from datetime import datetime
+from datetime import timezone
+from datetime import timedelta
 
 from flask import request
 from flask import render_template
 
 from app.main import main_blueprint
 from .forms import VotingForm
+from .decorators import time_limits
 
 
 @main_blueprint.route('/', methods=['GET', 'POST'])
+@time_limits(start_time=None,
+             end_time=datetime(year=2017, month=5, day=13, hour=15, minute=40,
+                               tzinfo=timezone(timedelta(hours=8))),
+             end_time_message='投票已经结束')
 def index():
     form = VotingForm()
     if request.method == 'POST':
@@ -26,5 +34,9 @@ def index():
 
 
 @main_blueprint.route('/result', methods=['GET'])
+@time_limits(start_time=datetime(year=2017, month=5, day=14, hour=22, minute=00,
+                                 tzinfo=timezone(timedelta(hours=8))),
+             end_time=None,
+             start_time_message='结果尚未公布')
 def result():
     pass
